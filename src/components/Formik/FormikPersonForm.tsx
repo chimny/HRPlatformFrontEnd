453a5ef6fbec4981c2c4e34607aa49357b6ae4ad
chimny <aleks713@gmail.com>
2022-10-27 09:28:17 +0200

    working with formik - submit

--- src/components/Formik/FormikPersonForm.tsx
+++ src/components/Formik/FormikPersonForm.tsx
@@ -3,7 +3,7 @@ import {Field, Form, useFormik} from 'formik';
 import * as yup from 'yup';
 import {Button, FormControl, FormHelperText, FormLabel, Select, TextField} from "@mui/material";
 import {updateForm} from "../../redux/slices/formSlice";
-import {positionList} from "../../data/positionList";
+import {positionList, positionObj} from "../../data/positionList";
 import MenuItem from "@mui/material/MenuItem";
 
 
@@ -32,105 +32,105 @@ const validationSchema = yup.object({
         .min(3, 'surname should include at least 3 signs!')
         .max(50, 'surname length is maximum 50')
         .required('salary is required'),
-    // position: yup
-    //     .string()
-    //     .min(3, 'surname should include at least 3 signs!')
-    //     .max(50, 'surname length is maximum 50')
-    //     .required('surname is required'),
+    position: yup
+        .string()
+        .required('please choose the position!'),
 });
 
 export const FormikPersonForm = () => {
     const formik = useFormik({
         initialValues: {
             name: '',
-            surname:'',
-            position:'',
-            salary:''
+            surname: '',
+            position: '',
+            salary: ''
         },
         validationSchema: validationSchema,
         // funkcja poniżej do wysyłki formularza
+
         onSubmit: (values) => {
-            alert(JSON.stringify(values, null, 2));
+            alert('sent')
         },
     });
 
+
     return (
         <div>
+            <form onSubmit={formik.handleSubmit}>
+                <TextField
+                    fullWidth
+                    id="name"
+                    name="name"
+                    label="name"
+                    value={formik.values.name}
+                    onChange={formik.handleChange}
+                    error={formik.touched.name && Boolean(formik.errors.name)}
+                    helperText={formik.touched.name && formik.errors.name}
+                />
+                <TextField
+                    fullWidth
+                    id="surname"
+                    name="surname"
+                    label="surname"
+                    value={formik.values.surname}
+                    onChange={formik.handleChange}
+                    error={formik.touched.surname && Boolean(formik.errors.surname)}
+                    helperText={formik.touched.surname && formik.errors.surname}
+                />
+
+                <TextField
+                    fullWidth
+                    id="salary"
+                    name="salary"
+                    label="salary"
+                    value={formik.values.salary}
+                    onChange={formik.handleChange}
+                    error={formik.touched.salary && Boolean(formik.errors.salary)}
+                    helperText={formik.touched.salary && formik.errors.salary}
+                />
 
 
+                {/*<FormControl className={'styledForm'} fullWidth={true}*/}
+                {/*             error={formik.touched.salary && Boolean(formik.errors.salary)}*/}
+                {/*>*/}
+                {/*    /!*@todo update form postinion*!/*/}
+                {/*    <FormLabel id="position-label">position</FormLabel>*/}
+                {/*    <Select*/}
+                {/*        error={formik.touched.position && Boolean(formik.errors.position)}*/}
+                {/*        labelId="position-label"*/}
+                {/*        // id="demo-simple-select"*/}
+                {/*        id="position"*/}
+                {/*        name="position"*/}
+                {/*        label="position"*/}
+                {/*        value={formik.values.position}*/}
 
-                    <form onSubmit={formik.handleSubmit}>
-                        <TextField
-                            fullWidth
-                            id="name"
-                            name="name"
-                            label="name"
-                            value={formik.values.name}
-                            onChange={formik.handleChange}
-                            error={formik.touched.name && Boolean(formik.errors.name)}
-                            helperText={formik.touched.name && formik.errors.name}
-                        />
-                        <TextField
-                            fullWidth
-                            id="surname"
-                            name="surname"
-                            label="surname"
-                            value={formik.values.surname}
-                            onChange={formik.handleChange}
-                            error={formik.touched.surname && Boolean(formik.errors.surname)}
-                            helperText={formik.touched.surname && formik.errors.surname}
-                        />
+                {/*        onChange={e => formik.handleChange(e.target.value)}*/}
+                {/*        renderValue={(selected) => {*/}
+                {/*            if (typeof selected === "string" && selected.length === 0) {*/}
+                {/*                return <em>Position</em>;*/}
+                {/*            }*/}
 
-                        <TextField
-                            fullWidth
-                            id="salary"
-                            name="salary"
-                            label="salary"
-                            value={formik.values.salary}
-                            onChange={formik.handleChange}
-                            error={formik.touched.salary && Boolean(formik.errors.salary)}
-                            helperText={formik.touched.salary && formik.errors.salary}
-                        />
+                {/*            return selected;*/}
+                {/*        }}*/}
+                {/*    >*/}
+
+                {/*        {positionObj.map(({key,value}) => {*/}
+                {/*            return (*/}
+                {/*                <MenuItem key={key}  value={value}>{value}</MenuItem>*/}
+                {/*            )*/}
+                {/*        })}*/}
 
 
-                        <FormControl className={'styledForm'} fullWidth={true}
-                                     error={formik.touched.salary && Boolean(formik.errors.salary)}
-                        >
-                            {/*@todo update form postinion*/}
-                            <FormLabel id="position-label">position</FormLabel>
-                            <Select
-                                error={formik.touched.salary && Boolean(formik.errors.salary)}
-                                labelId="position-label"
-                                id="demo-simple-select"
-                                value={formik.values.position}
-                                label="position"
-
-                                onChange={e => formik.handleChange(e.target.value)}
-                                renderValue={(selected) => {
-                                    if (typeof selected === "string" && selected.length === 0) {
-                                        return <em>Position</em>;
-                                    }
-
-                                    return selected;
-                                }}
-                            >
-
-                                {positionList.map((position) => {
-                                    return (
-                                        <MenuItem key={position} value={position}>{position}</MenuItem>
-                                    )
-                                })}
+                {/*    </Select>*/}
+                {/*    <FormHelperText>{formik.errors.position}</FormHelperText>*/}
+                {/*</FormControl>*/}
 
 
-                            </Select>
-                            <FormHelperText>{'test message'}</FormHelperText>
-                        </FormControl>
+                <Button color="primary" variant="contained" fullWidth type="submit" onClick={(e)=>formik.handleSubmit}>
+                    Submit
+                </Button>
 
-
-                        <Button color="primary" variant="contained" fullWidth type="submit">
-                            Submit
-                        </Button>
-                    </form>
+            </form>
 
         </div>
     );
--- src/data/positionList.ts
+++ src/data/positionList.ts
@@ -1,4 +1,29 @@
 import {PositionList} from "../../../backend/types/personPosition";
 
 
+interface positionObj{
+    key: PositionList,
+    value: PositionList
+}
+
+export const positionObj:positionObj[] = [{
+    key:'Trainee',
+    value:'Trainee',
+},{
+    key:'Assistant',
+    value:'Assistant',
+},{
+    key:'Junior Specialist',
+    value:'Junior Specialist',
+},{
+    key:'Specialist',
+    value:'Specialist',
+},{
+    key:'Senior Specialist',
+    value:'Senior Specialist',
+},{
+    key:'Manager',
+    value:'Manager',
+}];
+
 export const positionList: PositionList[] = ['Trainee', 'Assistant', 'Junior Specialist', 'Specialist', 'Senior Specialist', 'Manager']
