9c59a3ff536f95cf006d9a5e2a3041412558d5b5
chimny <aleks713@gmail.com>
2022-11-20 20:55:34 +0100

    front end done

--- src/components/Position/PositionList.tsx
+++ src/components/Position/PositionList.tsx
@@ -1,8 +1,21 @@
+import {useEffect, useState} from "react";
 
 export const PositionList = ()=>{
 
+    const [positionList,setPositionList] = useState(null);
+
+    const receiveHandler = async ()=>{
+        const receivedData =  await fetch('http://localhost:3001/positions');
+
+        console.log(await receivedData.json())}
+
+    // useEffect(receiveHandler,[]);
+
+
+    (receiveHandler)()
+
     return (
-        <h1>Position list listed with possibility to add / remove positions...</h1>
+        <h1>Position list with description</h1>
     )
 
 }
